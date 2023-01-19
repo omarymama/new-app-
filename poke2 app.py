b564ae28ab069fc24f13d0afb611e31d9a5af68f
omarymama <121270945+omarymama@users.noreply.github.com>
2023-01-20 04:42:52 +0900

    Update poke2 app.py

--- poke2 app.py
+++ poke2 app.py
@@ -13,3 +13,7 @@ st.write("Enter the name of a Pokemon:")
 name = st.text_input()
 
 if name:
+    pokemon = data[data['Name'] == name]
+    st.write(pokemon)
+else:
+    st.write("Please enter a Pokemon name.")
